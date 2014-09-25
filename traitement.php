19f7fa998cec6febed79b1ce33e35e4d3b720f00
gfevrier <g-fevrier-pro@orange.fr>
2014-09-25 13:56:40 +0200

    [Code] Modification du message final.

--- traitement.php
+++ traitement.php
@@ -1,3 +1,8 @@
 <?php
+
+/**
+* Ce fichier a été ajouté dans le repository GitHub
+*/
+
 // Chaîne au format JSON renvoyée par le script qui sera traitée par la fonction Javascript "resultatTraitement"
-echo "{success: true, msg : 'Les informations ont été traitées.'}";
+echo "{success: true, msg : 'Les informations ont été traitées, merci de votre patience.'}";
